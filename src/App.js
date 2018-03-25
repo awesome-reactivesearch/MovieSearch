518db404314052b7cc06439e6583140b561c1793
rajmeghpara <meghpararajkumar@gmail.com>
2018-03-25 18:44:25 +0530

    first update

--- src/App.js
+++ src/App.js
@@ -122,7 +122,10 @@ class App extends Component {
                                                                       componentClasses="tooltip-main" >
 
                                                                                                         <b className="result-title"  ><b>{res.title}</b></b>
-                                                                                                        <p className="rating"><b className="sub-title">★★★★★<span className="voters">({res.vote_count})</span> :</b><span className="subDescription" > {res.vote_average}/10 </span></p>
+                                                                                                        <p className="rating">
+                                                                                                              <b className="sub-title">★★★★★ </b>
+                                                                                                              <span className="voters">({res.vote_count})</span> :<span className="subDescription" > {res.vote_average}/10 </span>
+                                                                                                        </p>
                                                               </Tooltip>
                                                         </div>
                                                 </div>
